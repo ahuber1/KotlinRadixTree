1e8f1862a29354675487dc890a4f792e592661d8
Andrew Huber <andrewhuber@carfax.com>
2020-02-03 20:54:10 -0500

    Implemented getWords for KRadixTreeTests.kt

--- src/main/kotlin/com/ahuber/utils/Utils.kt
+++ src/main/kotlin/com/ahuber/utils/Utils.kt
@@ -13,8 +13,6 @@ inline val String.withQuotationMarks get() = "\"$this\""
 fun String.toCharArrayWithFill(newLength: Int, fillerChar: Char = '\u0000') : CharArray =
         CharArray(newLength) { index -> if (index < this.length) this[index] else fillerChar }
 
-
-
 fun Double.format(numDigits: Int) : String {
     check(numDigits >= 0) { "numDigits must be greater than or equal to zero" }
     return String.format("%.${numDigits}f", this)
@@ -31,4 +29,33 @@ fun String.repeat(n: Int) = this.repeat<String>(n).joinToString("")
 fun <T> T.repeat(n: Int) = when (n <= 0) {
     true -> emptySequence()
     false -> (0 until n).asSequence().map { this }
+}
+
+/**
+ * Returns the length of this [IntRange]
+ */
+inline val IntRange.length get() = this.last - this.first + 1
+
+/**
+ * Returns the midpoint of this [IntRange]
+ */
+inline val IntRange.middle get() = (this.first + this.last) / 2
+
+/**
+ * Returns an [IntRange] that starts at the same start point as the received [IntRange]
+ * and ends at the midpoint of the [IntRange] (exclusive)
+ * @see [middle]
+ */
+fun IntRange.halveLeft(): IntRange? = when (this.length) {
+    0 -> null
+    else -> this.first until this.middle
+}
+
+/**
+ * Returns an [IntRange] that starts at the midpoint of the received [IntRange] (inclusive) and ends at the
+ * endpoint of the received [IntRange] (inclusive)
+ */
+fun IntRange.halveRight(): IntRange? = when (this.length) {
+    0 -> null
+    else -> this.middle until this.last
 }
--- src/test/kotlin/com/ahuber/collections/KRadixTreeTests.kt
+++ src/test/kotlin/com/ahuber/collections/KRadixTreeTests.kt
@@ -1,23 +1,22 @@
 package com.ahuber.collections
 
 import com.ahuber.test.utils.getResourceAsFile
-import com.ahuber.utils.getLogger
+import com.ahuber.utils.halveLeft
+import com.ahuber.utils.halveRight
+import com.ahuber.utils.middle
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import kotlin.test.fail
 
 class KRadixTreeTests {
+    data class SizedSequence<T>(val sequence: Sequence<T>, val size: Int): Sequence<T> {
+        override fun iterator(): Iterator<T> = sequence.iterator()
+    }
+
     @Test
     fun `test with words`() {
-        val path = "words.txt"
-        val file = getResourceAsFile(path) ?: fail("Unable to get resource $path")
-        val words = file.readText()
-                .split(Regex("\\s"))
-                .asSequence()
-                .map { it.toLowerCase() }
-                .filter { it.isNotEmpty() }
-                .toSortedSet()
+        val words = getWords()
         val tree = KRadixTree()
         val targetWord = "abdications"
         var targetWordAdded = false
@@ -38,5 +37,66 @@ class KRadixTreeTests {
 
             assertEquals(index + 1, tree.size)
         }
+    }
+
+    private fun getWords(): SizedSequence<String> {
+        val path = "words.txt"
+        val file = getResourceAsFile(path) ?: fail("Unable to get resource $path")
+        var words: MutableList<String?> = file.readText()
+                .split(Regex("\\s"))
+                .asSequence()
+                .map { it.toLowerCase() }
+                .filter { it.isNotEmpty() }
+                .distinct()
+                .toMutableList()
+
+        val sequence = sequence<String> {
+            var leftRange: IntRange? = null
+            var rightRange: IntRange? = null
+
+            while (true) {
+                if (leftRange == null && rightRange == null) {
+                    words = words.filterNotNullTo(ArrayList())
+                }
+
+                leftRange = when (leftRange) {
+                    null -> words.indices.halveLeft()
+                    else -> leftRange.halveLeft()
+                }
+
+                rightRange = when (rightRange) {
+                    null -> words.indices.halveRight()
+                    else -> rightRange.halveRight()
+                }
+
+                if (leftRange == null && rightRange == null) {
+                    break
+                }
+
+                val leftValue = when (val leftMiddle = leftRange?.middle) {
+                    null -> null
+                    else -> words[leftMiddle].also { words[leftMiddle] = null }
+                }
+
+                val rightValue = when (val rightMiddle = rightRange?.middle) {
+                    null -> null
+                    else -> words[rightMiddle].also { words[rightMiddle] = null }
+                }
+
+                if (leftValue != null) {
+                    yield(leftValue)
+                }
+
+                if (rightValue != null) {
+                    yield(rightValue)
+                }
+            }
+
+            for (word in words.filterNotNull()) {
+                yield(word)
+            }
+        }
+
+        return SizedSequence(sequence, words.size)
     }
 }
