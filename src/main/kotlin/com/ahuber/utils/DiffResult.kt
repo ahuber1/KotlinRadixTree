80ba175ed03376de9024f75387ac41f826632a56
Andrew Huber <ahuber1@umbc.edu>
2020-03-03 09:05:00 -0500

    Improved diffWith algorithm

--- src/main/kotlin/com/ahuber/utils/DiffResult.kt
+++ src/main/kotlin/com/ahuber/utils/DiffResult.kt
@@ -1,5 +1,7 @@
 package com.ahuber.utils
 
+import kotlin.math.min
+
 sealed class DiffResult {
     object Different: DiffResult()
     data class Shared(val sharedPrefix: String, val remainder: String): DiffResult()
@@ -7,28 +9,19 @@ sealed class DiffResult {
 }
 
 fun String.diffWith(other: String): DiffResult {
-    if (this == other) {
-        return DiffResult.Identical
+    val minLength = min(this.length, other.length)
+    var endIndex = -1
+
+    for (index in 0 until minLength) {
+        if (this[index] != other[index]) break
+        endIndex = index
     }
 
-    var sharedPrefix = ""
-
-    for (length in 1..this.length) {
-        val substring = this.substring(0, length)
-
-        if (other.startsWith(substring)) {
-            sharedPrefix = substring
-        } else if (sharedPrefix.isNotEmpty()) {
-            break
-        }
+    return when {
+        endIndex == -1 -> DiffResult.Different
+        this.length == other.length && endIndex == this.lastIndex -> DiffResult.Identical
+        else -> DiffResult.Shared(this.substring(0..endIndex), this.substring(endIndex + 1))
     }
-
-    if (sharedPrefix.isEmpty()) {
-        return DiffResult.Different
-    }
-
-    val remainder = this.substring(sharedPrefix.length)
-    return DiffResult.Shared(sharedPrefix, remainder)
 }
 
 fun DiffResult.Shared.removeSharedPrefix(string: String): String? = when (string.startsWith(this.sharedPrefix)) {
